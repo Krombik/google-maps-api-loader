f45fcdb6426ee397585fd7fb1badb5265ddfc943
Andrii <jitelgor@gmail.com>
2024-05-08 23:20:56 +0200

    4.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,87 @@
-import type { GoogleMapsLoaderOptions, GoogleMapsLibrary } from "./types";
+import type {
+  GoogleMapsLoaderOptions,
+  GoogleMapsLibraries,
+  GoogleMapsLibrary,
+  ScriptOptions,
+  GoogleMapsLoaderStatus,
+} from './types';
 
-export type { GoogleMapsLoaderOptions, GoogleMapsLibrary };
-
-const enum _GoogleMapsLoaderStatus {
-  NONE,
-  LOADING,
-  LOADED,
-  ERROR,
-}
-
-export const GoogleMapsLoaderStatus: typeof _GoogleMapsLoaderStatus = {
-  NONE: 0,
-  LOADING: 1,
-  LOADED: 2,
-  ERROR: 3,
+export type {
+  GoogleMapsLoaderOptions,
+  GoogleMapsLoaderStatus,
+  GoogleMapsLibrary,
+  ScriptOptions,
 };
 
 declare class GoogleMapsLoader {
-  /** Current status of {@link GoogleMapsLoader} */
-  static readonly status: _GoogleMapsLoaderStatus;
+  /** @returns current status of {@link google.maps} script loading */
+  static getStatus(): GoogleMapsLoaderStatus;
+  /** @returns current status of a specific {@link library} loading */
+  static getStatus(library: GoogleMapsLibrary): GoogleMapsLoaderStatus;
+
   /**
-   * Promise of loading
+   * @returns promise of {@link google.maps} script loading completion
    *
-   * **Resolves** if {@link GoogleMapsLoader.load load} is success
+   * **Resolves** if {@link GoogleMapsLoader.load load} is successful
    *
    * **Rejects**
    *
    * - if {@link google.maps} was loaded outside of this library
-   * - if no `options` was set
+   * - if no `options` were set
    * - if script loading failed
    */
-  static readonly completion: Promise<void>;
+  static getCompletion(): Promise<void>;
+  /**
+   * @returns promise of loading the provided {@link library}
+   */
+  static getCompletion<L extends GoogleMapsLibrary>(
+    library: L
+  ): Promise<GoogleMapsLibraries[L]>;
+  /**
+   * @returns promise of loading provided {@link libraries}
+   */
+  static getCompletion<const A extends GoogleMapsLibrary[]>(
+    ...libraries: A
+  ): Promise<{
+    [Index in keyof A]: GoogleMapsLibraries[A[Index]];
+  }>;
+
   /**
    * Can be called multiple times, only on the first call it starts loading {@link google.maps} script with the given `options`
-   * @returns ‎{@link GoogleMapsLoader.completion completion}
    */
   static load(): Promise<void>;
+  /**
+   * loads {@link google.maps} script and a provided {@link library}
+   */
+  static load<L extends GoogleMapsLibrary>(
+    library: L
+  ): Promise<GoogleMapsLibraries[L]>;
+  /**
+   * loads {@link google.maps} script and provided {@link libraries}
+   */
+  static load<const A extends GoogleMapsLibrary[]>(
+    ...libraries: A
+  ): Promise<{
+    [Index in keyof A]: GoogleMapsLibraries[A[Index]];
+  }>;
+
+  /**
+   * @returns provided {@link library} or `undefined` if it's not loaded yet
+   */
+  static get<L extends GoogleMapsLibrary>(
+    library: L
+  ): GoogleMapsLibraries[L] | undefined;
+
+  /**
+   * @returns error for the provided {@link library} or `undefined`
+   */
+  static getError(
+    library: GoogleMapsLibrary
+  ):
+    | google.maps.MapsServerError
+    | google.maps.MapsNetworkError
+    | google.maps.MapsRequestError
+    | undefined;
 }
 
 type Identity<T> = T;
@@ -44,117 +90,213 @@ interface GoogleMapsLoader extends Identity<typeof GoogleMapsLoader> {
   /**
    * Sets options for the {@link GoogleMapsLoader}
    * @param options - options for {@link google.maps} script loading
+   * @param scriptOptions - options for script element
+   * @param callbackName - name for callback, which will be called when {@link google.maps} script loading ends, defaults to `"__gmlcb"`
    */
-  (options: GoogleMapsLoaderOptions): GoogleMapsLoader;
+  (
+    options: GoogleMapsLoaderOptions,
+    scriptOptions?: ScriptOptions,
+    callbackName?: string
+  ): GoogleMapsLoader;
 }
 
-const googleMapsLoader = ((): GoogleMapsLoader => {
-  let options!: GoogleMapsLoaderOptions;
+const enum Status {
+  NONE,
+  LOADING,
+  LOADED,
+  ERROR,
+}
 
-  let load!: () => Promise<void>;
+const googleMapsLoader = (() => {
+  let options: GoogleMapsLoaderOptions;
 
-  const CALLBACK_NAME = "__gmlcb";
+  let scriptOptions: ScriptOptions;
 
-  const completion = new Promise<void>((resolve, reject) => {
-    load = () => {
-      load = () => completion;
+  let callbackName: string;
 
-      if (typeof window != "undefined") {
-        let errorMessage: string | undefined;
-
-        if (!options) {
-          errorMessage = "no options was set";
-        } else if (window.google && google.maps) {
-          errorMessage = "google.maps already loaded";
-        }
-
-        if (errorMessage) {
-          GoogleMapsLoader.status = _GoogleMapsLoaderStatus.ERROR;
-
-          reject(new Error(errorMessage));
-        } else {
-          GoogleMapsLoader.status = _GoogleMapsLoaderStatus.LOADING;
-
-          const { async, defer, nonce, url, ...rest } = options;
-
-          const combinedUrl = (
-            Object.keys(rest) as Array<keyof typeof rest>
-          ).reduce(
-            (acc, key) => `${acc}&${key}=${rest[key]}`,
-            `${
-              url || "https://maps.googleapis.com/maps/api/js"
-            }?callback=${CALLBACK_NAME}`
-          );
-
-          const createScript = () => {
-            window.removeEventListener("online", createScript);
-
-            const script = document.createElement("script");
-
-            const { head } = document;
-
-            script.type = "text/javascript";
-
-            script.onerror = () => {
-              head.removeChild(script);
-
-              if (navigator.onLine) {
-                GoogleMapsLoader.status = _GoogleMapsLoaderStatus.ERROR;
-
-                reject(new Error("google.maps could not load"));
-              } else {
-                window.addEventListener("online", createScript);
-              }
-            };
-
-            if (defer != undefined) {
-              script.defer = defer;
-            }
-
-            if (async != undefined) {
-              script.async = async;
-            }
-
-            if (nonce != undefined) {
-              script.nonce = nonce;
-            }
-
-            script.src = combinedUrl;
-
-            head.appendChild(script);
-          };
-
-          window[CALLBACK_NAME as any] = (() => {
-            delete window[CALLBACK_NAME as any];
-
-            GoogleMapsLoader.status = _GoogleMapsLoaderStatus.LOADED;
-
-            resolve();
-          }) as any;
-
-          if (navigator.onLine) {
-            createScript();
-          } else {
-            window.addEventListener("online", createScript);
-          }
-        }
-      }
-
-      return completion;
-    };
-  });
-
-  const GoogleMapsLoader = (_options: GoogleMapsLoaderOptions) => {
+  const GoogleMapsLoader = (
+    _options: GoogleMapsLoaderOptions,
+    _scriptProps?: ScriptOptions,
+    _callbackName?: string
+  ) => {
     options = _options;
+
+    scriptOptions = _scriptProps || {};
+
+    callbackName = _callbackName || '__gmlcb';
 
     return GoogleMapsLoader;
   };
 
-  GoogleMapsLoader.status = _GoogleMapsLoaderStatus.NONE;
+  const completion = new Promise<void>((resolve, reject) => {
+    type Data = [
+      status: Status,
+      promise: Promise<any>,
+      lib: any,
+      error: any,
+      load: () => void,
+    ];
 
-  GoogleMapsLoader.completion = completion;
+    let status = Status.NONE;
 
-  GoogleMapsLoader.load = () => load();
+    const statuses = ['none', 'loading', 'loaded', 'error'] as const;
+
+    const store = new Map<GoogleMapsLibrary, Data>();
+
+    const handleOnline = (cb: () => void, ifOnline?: () => void) => {
+      if (navigator.onLine) {
+        (ifOnline || cb)();
+      } else {
+        window.addEventListener('online', cb, { once: true });
+      }
+    };
+
+    const initData = (key: GoogleMapsLibrary) => {
+      let load!: () => void;
+
+      const data: Data = [
+        Status.NONE,
+        new Promise((res, rej) => {
+          load = () => {
+            const reject = (err: any) => {
+              handleOnline(load, () => {
+                data[0] = Status.ERROR;
+
+                data[3] = err;
+
+                rej(err);
+              });
+            };
+
+            completion.then(() => {
+              google.maps.importLibrary(key).then((value) => {
+                data[0] = Status.LOADED;
+
+                data[2] = value;
+
+                res(value);
+              }, reject);
+            }, reject);
+          };
+        }),
+        undefined,
+        undefined,
+        load,
+      ];
+
+      store.set(key, data);
+
+      return data;
+    };
+
+    const createCompletionGetter =
+      (
+        getCompletion: (key: GoogleMapsLibrary) => Promise<any>,
+        onetime?: () => undefined
+      ) =>
+      (...libraries: GoogleMapsLibrary[]) => {
+        if (onetime) {
+          onetime = onetime();
+        }
+
+        const l = libraries.length;
+
+        return l
+          ? l > 1
+            ? Promise.all(libraries.map(getCompletion))
+            : getCompletion(libraries[0])
+          : completion;
+      };
+
+    (GoogleMapsLoader as GoogleMapsLoader).get = (key) =>
+      store.has(key) ? store.get(key)![2] : undefined;
+
+    (GoogleMapsLoader as GoogleMapsLoader).getError = (key) =>
+      store.has(key) ? store.get(key)![3] : undefined;
+
+    (GoogleMapsLoader as GoogleMapsLoader).getStatus = (
+      key?: GoogleMapsLibrary
+    ) =>
+      statuses[
+        key ? (store.has(key) ? store.get(key)![0] : Status.NONE) : status
+      ];
+
+    (GoogleMapsLoader as GoogleMapsLoader).getCompletion =
+      createCompletionGetter((key) => (store.get(key) || initData(key))[1]);
+
+    (GoogleMapsLoader as GoogleMapsLoader).load = createCompletionGetter(
+      (key) => {
+        const data = store.get(key) || initData(key);
+
+        if (!data[0]) {
+          data[0] = Status.LOADING;
+
+          handleOnline(data.pop()!);
+        }
+
+        return data[1];
+      },
+      () => {
+        let errorMessage: string | undefined;
+
+        if (!options) {
+          errorMessage = 'no options was set';
+        } else if (window.google && google.maps) {
+          errorMessage = 'google.maps already loaded';
+        }
+
+        if (errorMessage) {
+          status = Status.ERROR;
+
+          reject(new Error(errorMessage));
+        } else {
+          status = Status.LOADING;
+
+          const src = (
+            Object.keys(options) as Array<keyof GoogleMapsLoaderOptions>
+          ).reduce(
+            (acc, key) => `${acc}&${key}=${options[key]}`,
+            `https://maps.googleapis.com/maps/api/js?callback=${callbackName}&loading=async`
+          );
+
+          const createScript = () => {
+            const script = document.createElement('script');
+
+            script.type = 'text/javascript';
+
+            script.onerror = () => {
+              script.remove();
+
+              handleOnline(createScript, () => {
+                status = Status.ERROR;
+
+                reject(new Error('google.maps could not load'));
+              });
+            };
+
+            script.src = src;
+
+            for (const key in scriptOptions) {
+              // @ts-expect-error
+              script[key] = scriptOptions[key as keyof ScriptOptions]!;
+            }
+
+            document.head.appendChild(script);
+          };
+
+          window[callbackName as any] = (() => {
+            delete window[callbackName as any];
+
+            status = Status.LOADED;
+
+            resolve();
+          }) as any;
+
+          handleOnline(createScript);
+        }
+      }
+    );
+  });
 
   return GoogleMapsLoader as GoogleMapsLoader;
 })();
